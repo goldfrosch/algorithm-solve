35115fd28ee18b7f155566a59138674fce8615b7
GoldFrosch <goldfrosch@daum.net>
2024-04-02 09:53:49 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,33 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-//문제 https://www.acmicpc.net/problem/1463
+int N;
+int NL[1000001];
+
 int main() {
-	int num;
-	std::cin >> num;
-	vector<int> test(num + 1);
+    std::cin >> N;
 
-	//bottom-up
-	//처음에 2부터 각각 나눠지는 횟수를 구하면서 최소한의 값을 계속
-	//구하는 상태에서 1을 더하고 그 값과 비교해 계속 +하는 방식
-	test[1] = 0;
-	for (int i = 2; i <= num; i++) {
-		test[i] = test[i - 1] + 1;
-		if (!(i % 3)) {
-			/* cout << i << "," << min(test[i], test[i / 3] + 1);
-			printf("\n"); */
-			test[i] = min(test[i], test[i / 3] + 1);
-		}
-		if (!(i % 2)) {
-			/* cout << i << "," << min(test[i], test[i / 2] + 1);
-			printf("\n"); */
-			test[i] = min(test[i], test[i / 2] + 1);
-		}
-	}
+    NL[1] = 0;
+    for (int i = 2; i <= N; i++) {
+        NL[i] = NL[i - 1] + 1;
 
-	cout << test[num] << endl;
-	return 0;
+        if (i % 2 == 0) NL[i] = min(NL[i], NL[i / 2] + 1);
+        if (i % 3 == 0) NL[i] = min(NL[i], NL[i / 3] + 1);
+    }
+
+    std::cout << NL[N];
+
+    return 0;
 }
