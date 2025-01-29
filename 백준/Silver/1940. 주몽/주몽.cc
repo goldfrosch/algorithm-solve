3162c33b22ba8d84cec590cef7cf068a1254c54c
GoldFrosch <goldfrosch@daum.net>
2025-01-30 01:13:26 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,27 +1,49 @@
-
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-int n, m, s[15151], cnt;
+int List[15151];
+
 int main() {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> s[i];
-    sort(s, s + n);
-    int l = 0, r = n - 1;
-    while (l < r) {
-        if (s[l] + s[r] == m) {
-            l++;
-            r--;
-            cnt++;
+    // https://www.acmicpc.net/problem/1940
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    std::cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> List[i];
+    }
+
+    sort(List, List + N);
+
+    int Left = 0;
+    int Right = N - 1;
+    int Result = 0;
+
+    // 양 끝에서 하나씩 줄어들게 한다.
+    while (Left < Right) {
+        // 양쪽의 데이터를 합쳤을 때 M이 나온다면
+        if (List[Left] + List[Right] == M) {
+            // 각각 한칸씩 서로 당겨온다.
+            Left += 1;
+            Right -= 1;
+
+            Result += 1;
+
+            // 서로 끝을 더했을 때 M보다 큰 경우
+        } else if (List[Left] + List[Right] > M) {
+            Right -= 1;
         }
-        else if (m < s[l] + s[r]) {
-            r--;
-        }
+        // 그 외에는 왼쪽을 앞으로 한칸 밀어준다.
         else {
-            l++;
+            Left += 1;
         }
     }
-    cout << cnt;
+
+    printf("%d", Result);
+
     return 0;
 }
