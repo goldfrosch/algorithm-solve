06534ac3c11b95f01cb9d087c81c8469dd848c14
GoldFrosch <goldfrosch@daum.net>
2025-04-22 23:58:05 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // https://www.acmicpc.net/problem/10998
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int A, B;
+    std::cin >> A >> B;
+
+    printf("%d", A * B);
+
+    return 0;
+}
