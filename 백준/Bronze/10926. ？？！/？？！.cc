da62e93c5747850fe3a4b2ff945cc189fe9b7d07
GoldFrosch <goldfrosch@daum.net>
2025-05-07 21:50:46 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // https://www.acmicpc.net/problem/10926
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string N;
+    std::cin >> N;
+
+    printf("%s??!", N.c_str());
+
+    return 0;
+}
