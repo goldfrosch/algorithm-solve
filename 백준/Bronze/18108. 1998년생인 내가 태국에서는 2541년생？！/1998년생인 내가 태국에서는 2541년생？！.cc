c373687230a74c6fdb27156bcd4b7b4bfa4c06eb
GoldFrosch <goldfrosch@daum.net>
2025-05-11 00:55:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // https://www.acmicpc.net/problem/18108
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    std::cin >> N;
+
+    printf("%d", N - 543);
+
+    return 0;
+}
